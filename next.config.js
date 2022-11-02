2ea86783d1c6c2b843347d89ba7fc28babc1986d
Mohit Singh <mohit@mohitsingh.in>
2022-11-02 07:15:29 +0530

    chore: upgrade to next 13

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withMarkdoc = require('@markdoc/next.js')
 module.exports = withMarkdoc()({
   reactStrictMode: true,
   pageExtensions: ['js', 'md'],
-  experimental: {
-    newNextLinkBehavior: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 })
